dfa28ebb4ac0ccbdd468587fcf14bbdce2f03eba
zhubin215130 <zhubin215130@163.com>
2017-12-12 14:29:28 +0800

    Fix bug for Cnn for Text Classification

--- 11-4-CNN-Text-Classfication.py
+++ 11-4-CNN-Text-Classfication.py
@@ -5,8 +5,9 @@ import numpy as np
 import os
 import time
 import datetime
-import data_helpers
-from text_cnn import TextCNN
+
+from text_classification import data_helpers
+from text_classification.text_cnn import TextCNN
 from tensorflow.contrib import learn
 
 # Parameters
@@ -14,8 +15,8 @@ from tensorflow.contrib import learn
 
 # Data loading params
 tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
-tf.flags.DEFINE_string("positive_data_file", "./data/rt-polaritydata/rt-polarity.pos", "Data source for the positive data.")
-tf.flags.DEFINE_string("negative_data_file", "./data/rt-polaritydata/rt-polarity.neg", "Data source for the negative data.")
+tf.flags.DEFINE_string("positive_data_file", "./text_classification/data/rt-polaritydata/rt-polarity.pos", "Data source for the positive data.")
+tf.flags.DEFINE_string("negative_data_file", "./text_classification/data/rt-polaritydata/rt-polarity.neg", "Data source for the negative data.")
 
 # Model Hyperparameters
 tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
